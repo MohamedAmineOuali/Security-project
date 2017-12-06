477b8cb74940c29d88aeb3534c375d24be187c87
OussamaSghaier <oussamasghaier1995@gmail.com>
2017-12-07 00:40:22 +0100

    LDAP : Create Client + Get Client

--- shared/client.py
+++ shared/client.py
@@ -1,9 +1,14 @@
 class Client:
 
     def __init__(self,num,nom,prenom,login,password,certification=None):
-        self.num = num
-        self.nom = nom
-        self.prenom = prenom
-        self.login = login
+        self.num = int(num.__str__())
+        self.nom = nom.__str__()
+        self.prenom = prenom.__str__()
+        self.login = login.__str__()
         self.password = password
-        self.certification = certification
+        self.certification = certification.__str__()
+
+    def __str__(self):
+        return 'login:'+str(self.login)+'-'+str('nom:'+self.nom)+'-'+str('prenom:'+self.prenom)+'-'\
+               +'numTelephone:' + str(self.num) + '-'+'password:'+self.password\
+               +'-'+'certification:'+self.certification+'.'
--- shared/ldap.py
+++ shared/ldap.py
@@ -1,19 +1,40 @@
 from client import Client
-import ldap3
+from ldap3 import *
 
 
 class LDAP_server:
-    def __init__(self,uri='ldap://localhost',login = "cn=admin,dc=suse,dc=com",password = "Admin"):
-        self.server = ldap3.Server(uri)
-        self.connection = ldap3.Connection(self.server, user=login, password=password)
+    def __init__(self,uri='ldap://localhost',login = "cn=admin,dc=budgie,dc=tn",password = "oussamabudgie"):
+        self.server = Server(uri)
+        self.ldap_base = 'ou=users,dc=budgie,dc=tn'
+        self.connection = Connection(self.server, user=login, password=password, auto_bind=True)
+        # self.connection.bind()
+        # print(self.connection)
+
 
     def create(self,client:Client):
         # create a client in LDAP server
-        return client
+        #Return TRUE if a new entry for client is created
+        # Return FALSE if the entry already exists or is not created
+        classObjects=['inetOrgPerson','person']
+        return self.connection.add('uid={},{}'.format(client.login, self.ldap_base), classObjects,
+                      {'cn': client.nom, 'sn': client.prenom, 'userPassword': client.password,
+                       'telephoneNumber': client.num, 'description': client.certification})
+
+
 
     def findClient(self,login):
+        self.connection.search(self.ldap_base, '(uid='+login+')'
+                            , attributes=['uid', 'cn', 'sn', 'userPassword', 'telephoneNumber', 'description'])
+        values=self.connection.entries[0]
+        print(values)
 
-        search_base = 'dc=suse,dc=de'
+        return Client(values['telephoneNumber'], values['cn'], values['sn'], values['uid'],
+                      values['userPassword'].__str__().split('\'')[1], values['description'])
 
-        #self.connection.search(search_base, search_filter, attributes=attrs)
 
+# Test scénario , add client then get it from ldap server
+l=LDAP_server()
+client = Client(3333, 'cn3', 'sn3', 'uid3', 'pwd3', 'certif3')
+created=l.create(client)
+print('Is a new entry created ? %s'%created)
+print(l.findClient('uid2'))
