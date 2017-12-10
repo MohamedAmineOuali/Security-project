c2b9ee4f148feae4d651409ab41843d0ece0c472
OussamaSghaier <oussamasghaier1995@gmail.com>
2017-12-10 17:25:53 +0100

    Client : KeyPair + Request

--- client/functionalities.py
+++ client/functionalities.py
@@ -1,7 +1,10 @@
 
 from shared.openssl import *
 
+# Create public/private keys for a client
+def generate_KeyPair():
+    return create_keyPair(crypto.TYPE_RSA, 1024)
 
-
-
-
+#create client request
+def create_client_certif_request(name,key):
+    return create_certRequest(key, CN=name+'Certificate')
